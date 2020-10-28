7f2a7e091bbbfd930daa4595a3114b3424fafe49
Hermerson Araújo <ne.hermerson@gmail.com>
2020-10-28 10:55:32 -0300

    feat: create link crud

--- src/index.js
+++ src/index.js
@@ -2,40 +2,55 @@ const { GraphQLServer } = require('graphql-yoga');
 
 const links = [
   {
-    id: 'link-0',
+    id: 'link-1',
     url: 'www.howtographql.com',
     description: 'Fullstack tutorial for GraphQL',
   },
 ];
 
-const typeDefs = `
-  type Query {
-    info: String!
-    feed: [Link!]!
-  }
-
-  type Link {
-    id: ID!
-    description: String!
-    url: String!
-  }
-`;
+const idCount = links.length;
 
 const resolvers = {
   Query: {
     info: () => 'This is the API of a Hackernews clone',
     feed: () => links,
+    link: (_, agrs) => {
+      const link = links.find((l) => l.id === agrs.id);
+      return link;
+    },
   },
 
-  Link: {
-    id: (parent) => parent.id,
-    description: (parent) => parent.description,
-    url: (parent) => parent.url,
+  Mutation: {
+    post: (parent, agrs) => {
+      const link = {
+        id: `link-${idCount + 1}`,
+        description: agrs.description,
+        url: agrs.url,
+      };
+      links.push(link);
+      return link;
+    },
+
+    updateLink: (_, agrs) => {
+      const linkIndex = links.findIndex((l) => l.id === agrs.id);
+      const link = links[linkIndex];
+      link.description = agrs.description || link.description;
+      link.url = agrs.url || link.url;
+      links[linkIndex] = link;
+      return link;
+    },
+
+    deleteLink: (_, agrs) => {
+      const linkIndex = links.findIndex((l) => l.id === agrs.id);
+      const link = links[linkIndex];
+      links.splice(linkIndex, 1);
+      return link;
+    },
   },
 };
 
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: './src/schema.graphql',
   resolvers,
 });
 
