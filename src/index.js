9b5acc8d98c25398dd7d2e86e1d409dbaebe061a
Hermerson Araújo <ne.hermerson@gmail.com>
2020-10-28 10:30:46 -0300

    feat: add feed query

---
+++ src/index.js
@@ -0,0 +1,42 @@
+const { GraphQLServer } = require('graphql-yoga');
+
+const links = [
+  {
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    description: 'Fullstack tutorial for GraphQL',
+  },
+];
+
+const typeDefs = `
+  type Query {
+    info: String!
+    feed: [Link!]!
+  }
+
+  type Link {
+    id: ID!
+    description: String!
+    url: String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    info: () => 'This is the API of a Hackernews clone',
+    feed: () => links,
+  },
+
+  Link: {
+    id: (parent) => parent.id,
+    description: (parent) => parent.description,
+    url: (parent) => parent.url,
+  },
+};
+
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers,
+});
+
+server.start(() => console.log('Server is running on http://localhost:4000/'));
