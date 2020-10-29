335a9b8c9f89fa4a7f4ae2d03ef91cabb4b064c9
Hermerson Araújo <ne.hermerson@gmail.com>
2020-10-29 10:19:34 -0300

    feat: add pagination to feed resolver

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -6,7 +6,11 @@ const feed = async (_, args, context) => {
     ],
   } : {};
 
-  const links = await context.prisma.link.findMany({ where });
+  const links = await context.prisma.link.findMany({
+    where,
+    skip: args.skip,
+    take: args.take,
+  });
   return links;
 };
 
