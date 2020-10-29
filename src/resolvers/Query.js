41fd7aec756dac426377003c985ac099a7bebebd
Hermerson Araújo <ne.hermerson@gmail.com>
2020-10-29 10:14:33 -0300

    feat: create filter to feed resolver

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,4 +1,14 @@
-const feed = async (_, __, context) => context.prisma.link.findMany();
+const feed = async (_, args, context) => {
+  const where = args.filter ? {
+    OR: [
+      { description: { contains: args.filter } },
+      { url: { contains: args.filter } },
+    ],
+  } : {};
+
+  const links = await context.prisma.link.findMany({ where });
+  return links;
+};
 
 const link = async (_, args, context) => context.prisma.link.findOne({
   where: { id: args.id },
